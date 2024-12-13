3c16e0d0c0e32fdd53b225411a2eea3bc2d7feea
sSsamirRr-mine <sssamirrr4@gmail.com>
2024-12-13 14:51:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let firstName = prompt("ism")
+let tuman = prompt("tuman")
+let firstage = +prompt("age")
+let Email = prompt("email")
+
+console.log(` Ismi ${firstName} tumani ${tuman} yoshi ${firstage} emaili ${Email}  `);
+console.log(firstName + " " + tuman + " " + firstage + " " + Email);
+
+
+alert(firstName);
+alert(Email);
